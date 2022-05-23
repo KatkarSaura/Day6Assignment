2585089af0542d9047d64b1bf9a7437bf9eb37c1
Pooja <pooja.katkr@gmail.com>
2022-05-23 22:04:48 +0530

    Hello

---
+++ src/RevnoForloop.java
@@ -0,0 +1,16 @@
+
+public class RevnoForloop {
+
+public static void main(String[] args) {
+		
+		int num = 123456789, reversed = 0;
+		
+		for(;num !=0; num/=10){
+			int digit = num % 10;
+			reversed = reversed * 10 + digit;
+		}
+		System.out.println("Reversed Number: " + reversed);
+
+	}
+
+}
